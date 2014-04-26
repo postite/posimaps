21cc9460ed125ba274d06233cc56308e24420eec
postite <blog@postite.com>
2014-04-26 15:24:48 +0200

    try api

--- haxemaps/Api.hx
+++ haxemaps/Api.hx
@@ -1,12 +1,22 @@
-@:expose("mapapi")
+@:expose("Mapi")
+@:keepSub
 class Api
-{
-	function new()
+{	
+	public var pop:String="paf";
+	static var _instance:Api;
+	public var polo:Int=12;
+	private function new()
 	{
-		trace("yo")
+		trace("yo");
 	}
-	public function test()
+	public static  inline function getInstance():Api
+	{
+		if (_instance==null) _instance=new Api();
+		return _instance;
+	}
+	public  static function test()
 	{
 		return "op";
 	}
+	
 }
