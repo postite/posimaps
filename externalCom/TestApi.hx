7d4f6ae214f51b5d5ae23ff8df2240f11d32e046
postite <blog@postite.com>
2014-04-26 15:26:17 +0200

    external test

---
+++ externalCom/TestApi.hx
@@ -0,0 +1,23 @@
+
+//import Api;
+
+
+extern class Mapi{
+	public static function test():String;
+	public static function getInstance():#if display Api #else Dynamic #end;
+}
+
+class TestApi
+{
+
+	function new()
+	{
+		trace(" hello");
+		trace(" ------------" +Mapi.getInstance().polo);
+	}
+
+	static public function main()
+{
+		var app = new TestApi();
+	}
+}
